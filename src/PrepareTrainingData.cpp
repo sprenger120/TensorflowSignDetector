3c151997c7589d4310ab18fba75ce73fedcc338f
Maximilian Deubel <maximilian.deubel@gmail.com>
2018-06-26 21:05:30 +0200

    add new executable for classifier training data preparation

---
+++ src/PrepareTrainingData.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <opencv/cv.hpp>
+#include <opencv/highgui.h>
+//#include <opencv2/videoio.hpp>
+#include <fstream>
+#include <ctime>
+#include <string>
+#include <stdio.h>
+#include <ftw.h>
+#include <fnmatch.h>
+#include <algorithm>
+#include <string>
+
+#include "TrainingData.h"
+
+using namespace std;
+using namespace cv;
+
+int main() {
+  TrainingData::TrainingData trainingData;
+
+  cv::destroyAllWindows();
+  return 0;
+}
