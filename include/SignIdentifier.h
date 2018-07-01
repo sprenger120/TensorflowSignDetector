8b072faeeb0cc4de937e1baa132b0dd229da526e
Michael Albrecht <michael.albrecht@st.ovgu.de>
2018-07-01 05:28:52 +0200

    . see commit before prevoius

--- include/SignIdentifier.h
+++ include/SignIdentifier.h
@@ -15,11 +15,24 @@ public:
   SignIdentifier();
   virtual ~SignIdentifier();
 
-  const vector<SignPlace> detect(const cv::Mat input,  const cv::Rect& areaWithSigns);
+  const vector<SignPlace> detect(cv::Mat& input,  const cv::Rect& areaWithSigns);
 private:
-  void morphThinning(cv::Mat &img) const;
+  /**
+   * Seeks out pixel clusters and adds their rects to preFilteredRects
+   * Applies rules and methods stated in detect after ###################### break
+   * Input matrix will be altered!
+   * Won't add rects of the tiny size category
+   * @param mat
+   * @param preFilteredRects
+   * @param addPreEnlargedAswell true If the unenlarged rects should be added to preFilteredRects aswell
+   * @param onlyAddSmallCategory true if only signs from the small category should be added
+   */
+  void findPixelClustersFloodFillMethod(cv::Mat& mat, vector<cv::Rect>& preFilteredRects,
+      bool addPreEnlargedAswell, bool onlyAddSmallCategory);
 
-  vector<cv::Rect>  findAndDrawContours(const cv::Scalar& color, cv::Mat grayS, cv::Mat targetDraw);
+  cv::Point clipCoordinate(const cv::Point& pnt, const cv::Mat& clipMat) const;
+  cv::Rect clipRect(const cv::Rect& pnt, const cv::Mat& clipMat) const;
+  //vector<cv::Rect>  findPixelClustersContoursMethod(const cv::Scalar& color, cv::Mat grayS, cv::Mat targetDraw);
 };
 
 #endif //SIGN_DETECC_SIGNDETECTOR_H
